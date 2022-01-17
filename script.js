65447a632cb02361ae14819ac7d6ad8dab8ba1db
Ziddi-Mian <ahmadyounas893@gmail.com>
2022-01-17 15:56:22 +0500

    jgf

--- script.js
+++ script.js
@@ -246,7 +246,6 @@ console.log(diff_months(dt1, dt2));
 
 
 
-
 // Write a JavaScript function to get time differences in years between two dates.   
 // Test Data :
 // dt1 = new Date("June 13, 2014 08:11:00");
